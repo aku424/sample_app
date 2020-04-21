b725b79ec9b80f12ddec26b739d2d2c35c16919a
aku424tt <61079024+aku424tt@users.noreply.github.com>
2020-04-21 10:19:32 +0900

    [finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '238e9f7bf97caf555194b78a9c5ab1f29b59f1e4f7717ef7057e9e9d69463240aa9a391eed36b1498538e6b4dd79e1fa6e38dd5dfca8d35ecb00c35f4e7552cb'
